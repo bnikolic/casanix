1d71f29cb2e3f7f617e348d6a6868aa6bbedb788
Bojan Nikolic <bojan@bnikolic.co.uk>
2017-03-23 12:55:12 +0000

    Update rpfits to 2.24

--- pkgs/rpfits/default.nix
+++ pkgs/rpfits/default.nix
@@ -1,13 +1,13 @@
 { fetchurl, stdenv, gfortran }:
 
 stdenv.mkDerivation rec {
-  name = "rpfits-2.23";
+  name = "rpfits-2.24";
 
   buildInputs = [gfortran];
 
   src = fetchurl {
     url = "ftp://ftp.atnf.csiro.au/pub/software/rpfits/${name}.tar.gz" ;
-    sha256 = "1811y9iynzsb01p0589k5j0nww5imm3z829bs3l6w50n3faxkgkz";
+    sha256 = "09absmdk6bfnxavxwi1b3v196c8g1v1rswq4vxy34gh9n6gpa9gy";
   };
 
   sourceRoot="rpfits/linux";
